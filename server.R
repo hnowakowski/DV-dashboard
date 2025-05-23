70a833cc51ec3773c71e973302b1f1a7716fafc2
unknown <m.sattorov2005@gmail.com>
2025-05-23 08:10:34 +0200

    basic implementations

--- server.R
+++ server.R
@@ -1,6 +1,172 @@
 library(shiny)
 library(shinydashboard)
+library(leaflet)
+library(spData)      
+library(tidyr)
+library(ggplot2)
+library(dplyr)
 
-function(input, output){
+pop_data <- read.csv("datasets/population.csv", sep = ",")
+fer_data <- read.csv("datasets/fertility.csv", sep = ",")
+gdp_data <- read.csv("datasets/gdp.csv", sep = ",")
+
+world <- spData::world %>%
+  filter(name_long != "Antarctica")
+
+
+function(input, output, session) {
+
+
+  selected_country <- reactiveVal(NULL)
   
+  # --- POPULATION MAP ---
+  output$pop_map <- renderLeaflet({
+    leaflet(world) %>%
+      addProviderTiles("Esri.WorldTopoMap") %>%
+      addPolygons(layerId = ~name_long,
+                  fillColor = "lightblue", fillOpacity = 0.2,
+                  color = "black", weight = 1,
+                  label = ~name_long) %>%
+      setMaxBounds(lng1 = -180, lat1 = -90, lng2 = 180, lat2 = 90)
+  })
+  
+  observeEvent(input$pop_map_shape_click, {
+    selected_country(input$pop_map_shape_click$id)
+  })
+  
+  output$pop_count <- renderValueBox({
+    req(selected_country())
+    
+    p <- pop_data %>%
+      filter(Country.Name == selected_country()) %>%
+      slice(1) %>%
+      pull(X2023)
+    
+    shinydashboard::valueBox(
+      value = format(p, big.mark = ","),
+      subtitle = paste("Population of", selected_country()),
+      icon = icon("user"),
+      width = 14
+    )
+    
+  })
+  
+  
+  output$fertility_rate <- renderUI({
+    req(selected_country())
+
+  })
+  
+  output$pop_growth <- renderPlot({
+    req(selected_country())
+
+    pop_hist <- pop_data %>%
+      filter(Country.Name == selected_country()) %>%
+      pivot_longer(
+        cols = starts_with("X"),
+        names_to = "Year",
+        values_to = "Population"
+      ) %>%
+      mutate(Year = as.integer(sub("X", "", Year)))
+    
+    ggplot(pop_hist, aes(x = Year, y = Population)) +
+      geom_line(color = "blue") +
+      geom_point() +
+      labs(title = paste("Population Growth for", selected_country()),
+           x = "Year", y = "Population") +
+      theme_minimal()
+  })
+  
+  # --- GDP MAP ---
+  output$gdp_map <- renderLeaflet({
+    leaflet(world) %>%
+      addProviderTiles("Esri.WorldTopoMap") %>%
+      addPolygons(layerId = ~name_long,
+                  fillColor = "green", fillOpacity = 0.2,
+                  color = "black", weight = 1,
+                  label = ~name_long) %>%
+      setMaxBounds(lng1 = -180, lat1 = -90, lng2 = 180, lat2 = 90)
+  })
+  
+  observeEvent(input$gdp_map_shape_click, {
+    selected_country(input$gdp_map_shape_click$id)
+  })
+  
+  output$gdp_value <- renderValueBox({
+    req(selected_country())
+    
+    gdp <- gdp_data %>%
+      filter(Country.Name == selected_country()) %>%
+      slice(1) %>%
+      pull(X2023)
+    
+    gdp <- round(gdp/1000000000,1)
+    
+    shinydashboard::valueBox(
+      value = paste0(format(gdp, big.mark = ","), " billion"),
+      subtitle = paste("GDP of", selected_country()),
+      icon = icon("dollar-sign"),
+      width = 14,
+      color = "green"
+    )
+  })
+  
+  output$gdp_per_capita <- renderText({
+    req(selected_country())
+
+  })
+  
+  output$gdp_growth <- renderPlot({
+    req(selected_country())
+    
+    gdp_hist <- gdp_data %>%
+      filter(Country.Name == selected_country()) %>%
+      pivot_longer(
+        cols = starts_with("X"),
+        names_to = "Year",
+        values_to = "GDP"
+      ) %>%
+      mutate(
+        Year = as.integer(sub("X", "", Year)),
+        GDP = as.numeric(GDP) / 1e6
+        
+      )
+
+    ggplot(gdp_hist, aes(x = Year, y = GDP)) +
+      geom_line(color = "green") +
+      geom_point() +
+      labs(title = paste("GDP Growth for", selected_country()),
+           x = "Year", y = "GDP") +
+      theme_minimal()
+  })
+  
+  # --- QOL MAP ---
+  output$qol_map <- renderLeaflet({
+    leaflet(world) %>%
+      addProviderTiles("Esri.WorldTopoMap") %>%
+      addPolygons(layerId = ~name_long,
+                  fillColor = "orange", fillOpacity = 0.2,
+                  color = "black", weight = 1,
+                  label = ~name_long) %>%
+      setMaxBounds(lng1 = -180, lat1 = -90, lng2 = 180, lat2 = 90)
+  })
+  
+  observeEvent(input$qol_map_shape_click, {
+    selected_country(input$qol_map_shape_click$id)
+  })
+  
+  output$hdi_value <- renderText({
+    req(selected_country())
+
+  })
+  
+  output$happy_score <- renderText({
+    req(selected_country())
+
+  })
+  
+  output$hdi_growth <- renderPlot({
+    req(selected_country())
+    
+  })
 }
--- ui.R
+++ ui.R
@@ -1,31 +1,95 @@
 library(shiny)
 library(shinydashboard)
+library(leaflet)
+library(spData)
+library(dplyr)
 
-shinyUI(
-  dashboardPage(
-    dashboardHeader(title="people inspector idk"),
-    dashboardSidebar(
-      sidebarMenu(
-        menuItem("Population", tabName = "population"),
-        menuItem("GDP", tabName = "gdp"),
-        menuItem("Quality of Life", tabName = "qol")
+
+
+dashboardPage(
+  dashboardHeader(title="People Inspector"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Population", tabName = "population"),
+      menuItem("GDP", tabName = "gdp"),
+      menuItem("Quality of Life", tabName = "qol")
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "population",
+              fluidRow(
+                column(width=8,
+                       box(width=12, style="height: 90vh;",
+                           leafletOutput("pop_map", height = "90vh")
+                       )
+                ),
+                column(width=4,
+                       
+                       box(
+                         width = 14, style="height: 17vh;", 
+                        valueBoxOutput("pop_count", width = 16)
+                       )
+                       ,
+                       box(width=12, style="height: 28.5vh;",
+                           h4("Fertility Rate"),
+                           uiOutput("fertility_gauge")
+                       ),
+                       box(width=12, style="height: 40vh;",
+                           title = 'boopers',
+                           plotOutput("pop_growth", height = "290px")
+                       )
+                )
+                
+              )
+      ),
+      
+      tabItem(tabName = "gdp",
+              fluidRow(
+                column(width=8,
+                       box(width=12, style="height: 90vh;",
+                           leafletOutput("gdp_map", height = "90vh")
+                       )
+                ),
+                column(width=4,
+                       box(width=12, style="height: 17vh;", 
+                           valueBoxOutput("gdp_value")
+                       ),
+                       box(width=12, style="height: 28.5vh;",
+                           h4("GDP per Capita"),
+                           textOutput("gdp_per_capita")
+                       ),
+                       box(width=12, style="height: 40vh;",
+                           plotOutput("pop_growth", height = "290px")
+                       )
+                )
+              )
+      ),
+      
+      tabItem(tabName = "qol",
+              fluidRow(
+                column(width=8,
+                       box(width=12, style="height: 90vh;",
+                           leafletOutput("qol_map", height = "90vh")
+                       )
+                ),
+                column(width=4,
+                       box(width=12, style="height: 28.5vh;", 
+                           h4("HDI"),
+                           textOutput("hdi_value")
+                       ),
+                       box(width=12, style="height: 28.5vh;",
+                           h4("Happiness Score"),
+                           textOutput("happy_score")
+                       ),
+                       box(width=12, style="height: 28.5vh;",
+                           h4("HDI Over Time"),
+                           plotOutput("hdi_growth")
+                       )
+                )
+              )
       )
-    ),
-    dashboardBody(
-      fluidRow( # display flex does not work :(((((
-        column(width=8,
-               div(id="map_select_pop",
-                   box(width=12, style="height: 90vh;", "map here")
-                   )),
-        column(width=4,
-               div(id="value_box_pop", 
-                   box(width=12, style="height: 28.5vh;", "pop count here")),
-               div(id="gauge_pop",
-                   box(width=12, style="height: 28.5vh;", "fertility rate here")),
-               div(id="graph_pop",
-                   box(width=12, style="height: 28.5vh;", "pop graph here"))
-               )
-        )
-      )
+    )
   )
+
 )
