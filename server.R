3886db96653e787d3f39d528908cc16d674fefc2
unknown <m.sattorov2005@gmail.com>
2025-05-26 02:34:40 +0200

    datatables (jalfway)

--- server.R
+++ server.R
@@ -10,6 +10,7 @@ library(dplyr)
 library(countrycode)
 library(sf)
 library(plotly)
+library(DT)
 #library(rsconnect)
 
 pop_data <- read.csv("datasets/population.csv", sep = ",")
@@ -23,6 +24,32 @@ pop_data$Country.Code <- countrycode(pop_data$Country.Code, origin = "iso3c", de
 fer_data$Country.Code <- countrycode(fer_data$Country.Code, origin = "iso3c", destination = "iso2c") 
 gdp_data$Country.Code <- countrycode(gdp_data$Country.Code, origin = "iso3c", destination = "iso2c") 
 gdp_capita_data$Country.Code <- countrycode(gdp_capita_data$Country.Code, origin = "iso3c", destination = "iso2c") 
+
+
+
+
+error_plot <- function() {
+    error_message <- "No Info!"
+    ggplot() + 
+      annotate("text", x = 0.5, y = 0.5, label = error_message, size = 12, color = "black", hjust = 0.5, vjust = 0.5) +
+      theme_void()
+}
+
+error_value <- function(){
+  shinydashboard::valueBox(
+    value = "No information",
+    subtitle = "information of this country is not available",
+    icon = icon("skull", style = "color: white"),
+    width = 14,
+    color = "black"
+  )
+}
+
+error_gauge <- function() {
+  gauge(0, min = 0, max = 100, label = "No Info", sectors = gaugeSectors(success = c(0, 0)))
+}
+
+
 
 shinyServer(function(input, output, session) {
   print("running")
@@ -53,29 +80,34 @@ shinyServer(function(input, output, session) {
     p <- pop_data %>%
       filter(Country.Code == selected_country()) %>%
       pull(X2023)
+
     
-    
-    if (!(length(p) == 0)) {
-      if (!(is.na(p))){
-        balue = format(p, big.mark = ",")
-      } else {
-        balue = "undefined"
-      }
+    if ((length(p) == 0)) {
+      
+      error_value()
+      
+    } else if ((is.na(p))){
+      error_value()
+      
     } else {
-      balue = "undefined"
-    }
+      
+      balue = format(p, big.mark = ",")
+      
+      country_name <- pop_data %>%
+        filter(Country.Code == selected_country()) %>%
+        pull(Country.Name)
+      
+      shinydashboard::valueBox(
+        value = balue,
+        subtitle = paste("Population of", country_name),
+        icon = icon("user"),
+        color = "orange",
+        width = 14
+      )
     
-    country_name <- pop_data %>%
-      filter(Country.Code == selected_country()) %>%
-      pull(Country.Name)
+  }
     
-    shinydashboard::valueBox(
-      value = balue,
-      subtitle = paste("Population of", country_name),
-      icon = icon("user"),
-      color = "orange",
-      width = 14
-    )
+    
     
   })
   
@@ -91,7 +123,9 @@ shinyServer(function(input, output, session) {
     f <- fer_data %>%
       filter(Country.Code == selected_country()) %>%
       pull(X2023)
-  
+    if (length(f) == 0) {
+      error_gauge()
+    } else {
     # 2.1 is the replacement rate hence it's marked in green
     # basically the entire developed world is below that so the red threshold is 1.5 tho
     # so that not all western countries are marked in the red and there's moreso a distinction between whose situation is bad and whose is terrible
@@ -100,7 +134,7 @@ shinyServer(function(input, output, session) {
       warning = c(1.5, 2.1),
       danger = c(0, 1.5)
     ))
-
+    }
   })
   
   output$pop_growth <- renderPlot({
@@ -109,22 +143,25 @@ shinyServer(function(input, output, session) {
     country_name <- pop_data %>%
       filter(Country.Code == selected_country()) %>%
       pull(Country.Name)
-
-    pop_hist <- pop_data %>%
-      filter(Country.Code == selected_country()) %>%
-      pivot_longer(
-        cols = starts_with("X"),
-        names_to = "Year",
-        values_to = "Population"
-      ) %>%
-      mutate(Year = as.integer(sub("X", "", Year)))
-    
-    ggplot(pop_hist, aes(x = Year, y = Population)) +
-      geom_line(color = "blue") +
-      geom_point() +
-      labs(title = paste("Population Growth for", selected_country()),
-           x = "Year", y = "Population") +
-      theme_minimal()
+    if (length(country_name) == 0) {
+      error_plot()
+    } else {
+      pop_hist <- pop_data %>%
+        filter(Country.Code == selected_country()) %>%
+        pivot_longer(
+          cols = starts_with("X"),
+          names_to = "Year",
+          values_to = "Population"
+        ) %>%
+        mutate(Year = as.integer(sub("X", "", Year)))
+      
+      ggplot(pop_hist, aes(x = Year, y = Population)) +
+        geom_line(color = "blue") +
+        geom_point() +
+        labs(title = paste("Population Growth for", selected_country()),
+             x = "Year", y = "Population") +
+        theme_minimal()
+    }
   })
   
   # --- GDP MAP ---
@@ -151,30 +188,29 @@ shinyServer(function(input, output, session) {
     
     gdp <- round(gdp/1000000000,1)
     
-    if (!(length(gdp) == 0)) {
-      if (!(is.na(gdp))){
-        balue = paste0(format(gdp, big.mark = ","), " billion")
-        bolor = "green"
-      } else {
-        balue = "undefined"
-        bolor = "black"
-      }
+    if ((length(gdp) == 0)) {
+      
+      error_value()
+    
+    } else if ((is.na(gdp))){
+      error_value()
+      
     } else {
-      balue = "undefined"
-      bolor = "black"
+      balue = paste0(format(gdp, big.mark = ","), " billion")
+      bolor = "green"
+      
+      country_name <- gdp_data %>%
+        filter(Country.Code == selected_country()) %>%
+        pull(Country.Name)
+      
+      shinydashboard::valueBox(
+        value = balue,
+        subtitle = paste("GDP of", country_name),
+        icon = icon("dollar-sign"),
+        width = 14,
+        color = bolor
+      )
     }
-    
-    country_name <- gdp_data %>%
-      filter(Country.Code == selected_country()) %>%
-      pull(Country.Name)
-    
-    shinydashboard::valueBox(
-      value = balue,
-      subtitle = paste("GDP of", country_name),
-      icon = icon("dollar-sign"),
-      width = 14,
-      color = bolor
-    )
   })
   
   output$gdp_capita_gauge <- renderGauge({
@@ -188,20 +224,25 @@ shinyServer(function(input, output, session) {
     g <- gdp_capita_data %>%
       filter(Country.Code == selected_country()) %>%
       pull(X2023)
-    
-    # fictional tax haven countries make the gauge based on max gdp per capita useless (Japan is in the red)
-    # so the thresholds are more or less an educated assumption:
-    # Green: 30k and up -> most western countries like Japan 33k, Germany 54k, France 44k, usa 82k
-    # Yellow: 10k - 30k -> most *somewhat* developed countries like Poland 22k, r*ssia 13k (emphasis on somewhat developed), China 12k, Mexico 13k
-    # Red: below 10k -> most developing countries like basically most of sub-saharan Africa
-    # print(max_gdp_capita)
-    
-    # max gauge value set a lil above usa's gdp per capita so that the bar does not look comically small for most countries
-    gauge(g, min=0, max = 90000, symbol = '$', gaugeSectors(
-      success = c(30000, 90000),
-      warning = c(10000, 30000),
-      danger = c(0, 10000)
-    ))
+    if (length(g) != 0) { 
+        # fictional tax haven countries make the gauge based on max gdp per capita useless (Japan is in the red)
+        # so the thresholds are more or less an educated assumption:
+        # Green: 30k and up -> most western countries like Japan 33k, Germany 54k, France 44k, usa 82k
+        # Yellow: 10k - 30k -> most *somewhat* developed countries like Poland 22k, r*ssia 13k (emphasis on somewhat developed), China 12k, Mexico 13k
+        # Red: below 10k -> most developing countries like basically most of sub-saharan Africa
+        # print(max_gdp_capita)
+        
+        # max gauge value set a lil above usa's gdp per capita so that the bar does not look comically small for most countries
+        gauge(g, min=0, max = 90000, symbol = '$', gaugeSectors(
+          success = c(30000, 90000),
+          warning = c(10000, 30000),
+          danger = c(0, 10000)
+        ))
+        
+      } else {
+        error_gauge() 
+      }
+      
   })
   
   output$gdp_growth <- renderPlot({
@@ -211,25 +252,32 @@ shinyServer(function(input, output, session) {
       filter(Country.Code == selected_country()) %>%
       pull(Country.Name)
     
-    gdp_hist <- gdp_data %>%
-      filter(Country.Code == selected_country()) %>%
-      pivot_longer(
-        cols = starts_with("X"),
-        names_to = "Year",
-        values_to = "GDP"
-      ) %>%
-      mutate(
-        Year = as.integer(sub("X", "", Year)),
-        GDP = as.numeric(GDP) / 1e6
-        
-      )
-
-    ggplot(gdp_hist, aes(x = Year, y = GDP)) +
-      geom_line(color = "green") +
-      geom_point() +
-      labs(title = paste("GDP Growth for", country_name),
-           x = "Year", y = "GDP") +
-      theme_minimal()
+    if (length(country_name) != 0){
+      
+        gdp_hist <- gdp_data %>%
+          filter(Country.Code == selected_country()) %>%
+          pivot_longer(
+            cols = starts_with("X"),
+            names_to = "Year",
+            values_to = "GDP"
+          ) %>%
+          mutate(
+            Year = as.integer(sub("X", "", Year)),
+            GDP = as.numeric(GDP) / 1e6
+            
+          )
+      
+        ggplot(gdp_hist, aes(x = Year, y = GDP)) +
+          geom_line(color = "green") +
+          geom_point() +
+          labs(title = paste("GDP Growth for", country_name),
+               x = "Year", y = "GDP") +
+          theme_minimal()
+    
+      } else {
+            error_plot()
+      }
+    
   })
   
   # --- QOL MAP ---
@@ -254,46 +302,48 @@ shinyServer(function(input, output, session) {
       filter(flagCode == selected_country()) %>%
       pull(HumanDevelopmentIndex_HDI_score_2023)
     
-    hdi_c <- hdi_data %>%
-      filter(flagCode == selected_country()) %>%
-      pull(HumanDevelopmentIndex_HDITierCurrent_txt_YearFree)
-    
-    country_name <- hdi_data %>%
-      filter(flagCode == selected_country()) %>%
-      pull(country)
-    
-    if (!(length(hdi) == 0)) {
-      if (hdi_c == "Very High") {
-        balue <- format(hdi, big.mark = ",")
-        bicon <- icon("laugh")
-        bolor <- "green"
-      } else if (hdi_c == "High") {
-        balue <- format(hdi, big.mark = ",")
-        bicon <- icon("smile")
-        bolor <- "yellow"
-      } else if (hdi_c == "Medium") {
-        balue <- format(hdi, big.mark = ",")
-        bicon <- icon("meh")
-        bolor <- "orange"
-      } else {
-        balue <- format(hdi, big.mark = ",")
-        bicon <- icon("frown")
-        bolor <- "red"
-      }
-    } else {
-      balue <- "Undefined"
-      bicon <- icon("skull", style = "color:white")
-      bolor <- "black"
-    }
-    
-    shinydashboard::valueBox(
-      value = balue,
-      subtitle = paste("HDI of", country_name),
-      icon = bicon,
-      width = 14,
-      color = bolor
+    if (length(hdi) != 0) {
       
-    )
+      hdi_c <- hdi_data %>%
+        filter(flagCode == selected_country()) %>%
+        pull(HumanDevelopmentIndex_HDITierCurrent_txt_YearFree)
+      
+      country_name <- hdi_data %>%
+        filter(flagCode == selected_country()) %>%
+        pull(country)
+      
+      if (!(length(hdi) == 0)) {
+        if (hdi_c == "Very High") {
+          balue <- format(hdi, big.mark = ",")
+          bicon <- icon("laugh")
+          bolor <- "green"
+        } else if (hdi_c == "High") {
+          balue <- format(hdi, big.mark = ",")
+          bicon <- icon("smile")
+          bolor <- "yellow"
+        } else if (hdi_c == "Medium") {
+          balue <- format(hdi, big.mark = ",")
+          bicon <- icon("meh")
+          bolor <- "orange"
+        } else {
+          balue <- format(hdi, big.mark = ",")
+          bicon <- icon("frown")
+          bolor <- "red"
+        }
+      }
+      
+      shinydashboard::valueBox(
+        value = balue,
+        subtitle = paste("HDI of", country_name),
+        icon = bicon,
+        width = 14,
+        color = bolor
+        
+      )
+      
+    } else {
+        error_value()
+    }
 
   })
   
@@ -304,72 +354,127 @@ shinyServer(function(input, output, session) {
       filter(flagCode == selected_country()) %>%
       pull(country)
     
-    all_hap <- hap_data %>%
-      pull(HappiestCountriesWorldHappinessReportScore2024)
+    if (length(country_name) != 0) {
     
-    max_hap <- max(all_hap, na.rm = TRUE)
-    
-    h <- hap_data %>%
-      filter(flagCode == selected_country()) %>%
-      pull(HappiestCountriesWorldHappinessReportScore2024)
-    
-    
-    gauge(h, min=0, max = max_hap, gaugeSectors(
-      success = c(max_hap*0.7, max_hap),
-      warning = c(max_hap*0.5, max_hap*0.7),
-      danger = c(0, max_hap*0.5)
-    ))
+      all_hap <- hap_data %>%
+        pull(HappiestCountriesWorldHappinessReportScore2024)
+      
+      max_hap <- max(all_hap, na.rm = TRUE)
+      
+      h <- hap_data %>%
+        filter(flagCode == selected_country()) %>%
+        pull(HappiestCountriesWorldHappinessReportScore2024)
+      
+      
+      gauge(h, min=0, max = max_hap, gaugeSectors(
+        success = c(max_hap*0.7, max_hap),
+        warning = c(max_hap*0.5, max_hap*0.7),
+        danger = c(0, max_hap*0.5)
+      ))
+    } else {
+      error_gauge()
+    }
 
   })
   
-  library(plotly)
+  
   
   output$hdi_growth <- renderPlotly({
     req(selected_country())
     
     target_index <- which(hap_data$flagCode == selected_country())
+    
     if (length(target_index) != 0) {
-      target_pos <- hap_data[target_index, "HappiestCountriesWorldHappinessReportRankings2024"]
-      print(paste("Target index:", target_index))
       
-      window <- 2
-      neighbors <- hap_data[max(1, target_index - window) : min(target_index + window, nrow(hap_data)), ]
-      
-      neighbors$color_group <- ifelse(neighbors$HappiestCountriesWorldHappinessReportRankings2024 == target_pos, "red", "#1f77b4")
-      
-      lowest <- min(neighbors$HappiestCountriesWorldHappinessReportScore2024, na.rm = TRUE)
-      highest <- max(neighbors$HappiestCountriesWorldHappinessReportScore2024, na.rm = TRUE)
-      print(paste("Lowest score:", lowest))
-      print(paste("Highest score:", highest))
-      
-      p <- ggplot(neighbors, aes(x = reorder(country, HappiestCountriesWorldHappinessReportScore2024), 
-                                 y = HappiestCountriesWorldHappinessReportScore2024, 
-                                 fill = color_group,
-                                 text = paste0(country, "<br>Score: ", HappiestCountriesWorldHappinessReportScore2024, "<br>Rank: ", HappiestCountriesWorldHappinessReportRankings2024))) +
-        geom_col() +
-        scale_fill_identity() +
-        coord_flip(ylim = c(lowest-0.02, highest+0.02)) +
-        labs(title = "Happiness Score by Country",
-             x = "Country",
-             y = "Happiness Score") +
-        theme_minimal() + theme(
-          legend.position = "none",      
-          axis.title.x = element_blank(),
-          axis.title.y = element_blank(), 
-          axis.text.y = element_text(size = 8, margin = margin(r = 200)) 
-        )
-      rank <- as.character(target_pos)
-      ggplotly(p, tooltip = "text")
+          target_pos <- hap_data[target_index, "HappiestCountriesWorldHappinessReportRankings2024"]
+          print(paste("Target index:", target_index))
+          
+          window <- 2
+          neighbors <- hap_data[max(1, target_index - window ) : min(target_index + window, nrow(hap_data)), ]
+          
+          neighbors$color_group <- ifelse(neighbors$HappiestCountriesWorldHappinessReportRankings2024 == target_pos, "red", "#1f77b4")
+          
+          lowest <- min(neighbors$HappiestCountriesWorldHappinessReportScore2024, na.rm = TRUE)
+          highest <- max(neighbors$HappiestCountriesWorldHappinessReportScore2024, na.rm = TRUE)
+
+          neighbors$country <- gsub(" ", "\n", neighbors$country)
+          
+          p <- ggplot(neighbors, aes(x = reorder(country, HappiestCountriesWorldHappinessReportScore2024), 
+                                     y = HappiestCountriesWorldHappinessReportScore2024, 
+                                     fill = color_group,
+                                     text = paste0(country, "<br>Score: ", HappiestCountriesWorldHappinessReportScore2024, "<br>Rank: ", HappiestCountriesWorldHappinessReportRankings2024))) +
+            geom_col() +
+            scale_fill_identity() +
+            coord_flip(ylim = c(lowest-0.02, highest+0.02)) +
+            labs(title = "",
+                 x = "Country",
+                 y = "Happiness Score") +
+            theme_minimal() + theme(
+              legend.position = "none",
+              title = element_blank(),
+              axis.title.x = element_blank(),
+              axis.title.y = element_blank(), 
+              axis.text.y = element_text(size = 8, margin = margin(r = 200)) 
+            )
+          
+          rank <- as.character(target_pos)
+          ggplotly(p, tooltip = "text")
       
     } else {
-      error_message <- "No Info!"
-      
-      p <- ggplot() + 
-        annotate("text", x = 0.5, y = 0.5, label = error_message, size = 12, color = "black", hjust = 0.5, vjust = 0.5) +
-        theme_void()
-      
+      p <- error_plot()
       ggplotly(p)
-    }
+      }
   })
+  
+  
+  
+  
+  output$pop_table <- renderDT({
+    switch(input$table_choice,
+           "pop" = datatable(pop_data),
+           "fer" = datatable(fer_data)
+    )
+  })
+  
+  output$gdp_table <- renderDT({ 
+    datatable(
+      gdp_data,
+      options = list(
+        pageLength = 10,
+        autoWidth = TRUE
+      ),
+      rownames = FALSE
+    )
+    
+    
+    
+  })
+  
+  output$qol_table <- renderDT({ 
+    datatable(
+      hap_data,
+      options = list(
+        pageLength = 10,
+        autoWidth = TRUE
+      ),
+      rownames = FALSE
+    )
+    
+    
+    
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 })
--- ui.R
+++ ui.R
@@ -11,29 +11,58 @@ library(countrycode)
 library(sf)
 library(shinythemes)
 library(plotly)
-
+library(DT)
 
 dashboardPage(skin = "black",
   dashboardHeader(title="People Inspector"),
+  
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Introduction", tabName = "introduction"),
-      menuItem("Population", tabName = "population"),
-      menuItem("GDP", tabName = "gdp"),
-      menuItem("Quality of Life", tabName = "qol")
+      menuItem("Introduction", tabName = "introduction", icon = icon("info-circle")),
+      menuItem("Population", tabName = "population", icon = icon("users")),
+      menuItem("GDP", tabName = "gdp", icon = icon("dollar-sign")),
+      menuItem("Quality of Life", tabName = "qol", icon = icon("smile")),
+      
+      menuItem("Tables", icon = icon("table"),
+               menuSubItem("Population Table", tabName = "Population_table"),
+               menuSubItem("GDP Table", tabName = "GDP_table"),
+               menuSubItem("Quality of Life Table", tabName = "qol_table")
+      )
     )
-  ),
+  )
+  ,
   dashboardBody(
     tags$head(
       includeCSS("www/style.css")
     ),
+    
+    tags$style(HTML("
+          table.dataTable {
+            background-color: #1e1e1e !important;  
+            color: white !important;               
+          }
+        
+          table.dataTable thead {
+            background-color: #333333 !important;  
+            color: white !important;
+          }
+        
+          table.dataTable tbody tr {
+            background-color: #2a2a2a !important;
+          }
+        
+          .dataTables_wrapper {
+            background-color: #1e1e1e !important;
+            color: white !important;
+          }
+      ")),
+    
     tabItems(
       tabItem(tabName = "introduction",
               h2("Welcome to the People Inspector Dashboard"),
               p("This dashboard provides insights into population, GDP, and quality of life across countries."),
               p("Use the sidebar to navigate between different sections.")
-      )
-      ,
+      ),
       tabItem(tabName = "population",
               fluidRow(
                 column(width=8,
@@ -44,11 +73,11 @@ dashboardPage(skin = "black",
                 column(width=4,
                        
                        box(
-                         width = 12, 
+                         width = 12, style="height: 17vh;",
                           valueBoxOutput("pop_count", width = 16)
                        )
                        ,
-                       box(width=12,
+                       box(width=12, style="height: 25vh;",
                            h4("Fertility Rate"),
                            gaugeOutput("fertility_gauge")
                        ),
@@ -69,10 +98,10 @@ dashboardPage(skin = "black",
                        )
                 ),
                 column(width=4,
-                       box(width=12, 
+                       box(width=12, style="height: 17vh;",
                            valueBoxOutput("gdp_value")
                        ),
-                       box(width=12,
+                       box(width=12, style="height: 25vh;",
                            h4("GDP per Capita"),
                            gaugeOutput("gdp_capita_gauge")
                        ),
@@ -91,10 +120,10 @@ dashboardPage(skin = "black",
                        )
                 ),
                 column(width=4,
-                       box(width=12,
+                       box(width=12, style="height: 17vh;",
                            valueBoxOutput("hdi_value")
                        ),
-                       box(width=12,
+                       box(width=12, style="height: 25vh;",
                            h4("Happiness Score"),
                            gaugeOutput("happy_score")
                        ),
@@ -103,8 +132,23 @@ dashboardPage(skin = "black",
                            plotlyOutput("hdi_growth")
                        )
                 )
-              )
-      )
+            )
+      ),
+      
+      tabItem(tabName = "Population_table", fluidRow(
+        column(4,
+               selectInput("Pop choice", "Choose table:",
+                           choices = c("Population" = "pop", "Fertility" = "fer"))
+        ),
+        column(12,
+               DTOutput("pop_table")
+        )
+      )),   
+      
+      tabItem(tabName = "GDP_table", h2("GDP Panel"), DTOutput("gdp_table")),
+      tabItem(tabName = "qol_table", h2("Quality of Life Panel"), DTOutput("qol_table"))
+      
+      
     )
   )
 
